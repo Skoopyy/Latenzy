619aa8922c85b208c1e65ba25093ba2e02ec1f46
Skoopyy <104071935+Skoopyy@users.noreply.github.com>
2022-05-30 21:42:30 +0700

    Add files via upload

--- LatenzyInstaller.bat
+++ LatenzyInstaller.bat
@@ -51,8 +51,6 @@ echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\Latenzy Launcher.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "C:\Program Files\Latenzy\LatenzyLauncher.bat" >> CreateShortcut.vbs
-echo lnk.IconLocation = "C:\Program Files\Latenzy\LatenzyIcon.ico, 2" >> CreateShortcut.vbs
-echo lnk.Description = "EzPing but for free" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cls
 echo Created script to make desktop shortcut.
@@ -68,8 +66,6 @@ echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%HOMEDRIVE%%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Latenzy Launcher.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "C:\Program Files\Latenzy\LatenzyLauncher.bat" >> CreateShortcut.vbs
-echo lnk.IconLocation = "C:\Program Files\Latenzy\LatenzyIcon.ico, 2" >> CreateShortcut.vbs
-echo lnk.Description = "EzPing but for free" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs

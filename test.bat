5f2a720d2abf9b33538e6727fa157b1ce10351c9
Skoopyy <104071935+Skoopyy@users.noreply.github.com>
2023-02-18 20:54:02 +0700

    Delete test.bat

--- test.bat
+++
@@ -1,6 +0,0 @@
-if not "%1"=="am_admin" (
-    title Latenzy - Requesting admin permissions...
-    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
-    exit /b
-)
-echo hi
